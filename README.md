6642cf8f78579f20f4354005dde90cbe70f043ac
mninc <manicminer106@gmail.com>
2020-10-17 15:05:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# gladiator.tf script
+
+A script for owners of bots on [Gladiator.tf](https://gladiator.tf).
+
+## Installation
+
+1. Install [Greasemonkey (Firefox)](http://www.greasespot.net/) or [Tampermonkey (Chrome)](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
+2. Open the [script in raw view](https://github.com/mninc/gladiator.tf-bot-owner-script/raw/master/gladiator.user.js).
+3. Install.
---
+++ gladiator.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Gladiator.tf bot owner script
+// @namespace    https://steamcommunity.com/profiles/76561198320810968
+// @version      1.0
+// @description  A script for owners of bots on gladiator.tf
+// @author       manic
+// @grant        none
+// @license      MIT
+
+// @homepageURL     https://github.com/mninc/gladiator.tf-bot-owner-script
+// @supportURL      https://github.com/mninc/gladiator.tf-bot-owner-script/issues
+// @downloadURL     https://github.com/mninc/gladiator.tf-bot-owner-script/raw/master/gladiator.user.js
+
+// @run-at       document-end
+// @match        https://backpack.tf/stats*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+        $('.price-boxes').append(
+            `<a class="price-box" href="https://gladiator.tf/manage/my/item/${$('.stats-header-title').text().trim()}" target="_blank" data-tip="top" data-original-title="Gladiator.tf">
+                <img src="https://gladiator.tf/favicon-96x96.png" alt="gladiator">
+                <div class="text" style="display: flex; flex-direction: column; align-items: center; justify-content: center; margin-top: 0;">
+                    <div class="value" style="font-size: 14px;">Add on Gladiator.tf</div>
+                </div>
+            </a>`);
+})();
