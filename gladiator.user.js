9c20c986798cc999ceea1086228c804900abb0ed
mninc <manicminer106@gmail.com>
2021-01-07 12:53:31 +0000

    fix ? in item name

--- gladiator.user.js
+++ gladiator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gladiator.tf bot owner script
 // @namespace    https://steamcommunity.com/profiles/76561198320810968
-// @version      1.2
+// @version      1.3
 // @description  A script for owners of bots on gladiator.tf
 // @author       manic
 // @grant        none
@@ -19,7 +19,7 @@
     'use strict';
 
     $('.price-boxes').append(
-        `<a class="price-box" href="https://gladiator.tf/manage/my/item/${$('.stats-header-title').text().trim().replace("%", "%25")}/add" target="_blank" data-tip="top" data-original-title="Gladiator.tf">
+        `<a class="price-box" href="https://gladiator.tf/manage/my/item/${encodeURIComponent($('.stats-header-title').text().trim())}/add" target="_blank" data-tip="top" data-original-title="Gladiator.tf">
             <img src="https://gladiator.tf/favicon-96x96.png" alt="gladiator">
             <div class="text" style="display: flex; flex-direction: column; align-items: center; justify-content: center; margin-top: 0;">
                 <div class="value" style="font-size: 14px;">Add on Gladiator.tf</div>
